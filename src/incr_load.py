84cb1079e534259a4809912b73383914350269f3
Obicnwokolo <nwokolo.obinna10@gmail.com>
2025-01-06 13:03:56 +0000

    commit2

--- src/incr_load.py
+++ src/incr_load.py
@@ -8,11 +8,10 @@ max_cust_id = max_cust_id.collect()[0][0]
 
 query = 'SELECT * FROM obi_shopping_trend WHERE customer_id >' + str(max_cust_id)
 
-new_records = count(query)
-
 more_data = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").option("query", query).load()
 
-more_data.write.mode("append").saveAsTable("bigdata_nov_2024.obi_shopping_trend")
+new_records = count(more_data)
 
+more_data.write.mode("append").saveAsTable("bigdata_nov_2024.obi_shopping_trend")
 
 print(f"{new_records} new records successfully Loaded:" )
