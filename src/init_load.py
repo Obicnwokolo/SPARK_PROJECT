44e75f282a7f37e09abadfac6d815886a06c2d50
Obicnwokolo <nwokolo.obinna10@gmail.com>
2025-01-06 11:57:57 +0000

    commit2

--- src/init_load.py
+++ src/init_load.py
@@ -6,5 +6,5 @@ spark = SparkSession.builder.master("local").appName("Initial load").enableHiveS
 df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("dbtable", "obi_shopping_trend").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").load()
 df.printSchema()
 
-# df.write.mode("overwrite").saveAsTable("bigdata_nov_2024.obi_shopping_trend")
-# print("Successfully Load to Hive")
+df.write.mode("overwrite").saveAsTable("bigdata_nov_2024.obi_shopping_trend")
+print("Successfully Load to Hive")
